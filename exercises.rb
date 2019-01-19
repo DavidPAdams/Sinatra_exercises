f3a30f360e8e0b5dbe50b7663ba9b56329bcd39b
David Adams <david.riding.rails@gmail.com>
2019-01-18 22:40:23 -0700

    Modify main exercises file

--- exercises.rb
+++ exercises.rb
@@ -1,11 +1,11 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
-require_relative 'caesar_cipher/'+whatever the app file is called
-require_relative 'number_guess/'+whatever the app file is called
-require_relative 'word_guess'+whatever the app file is called
+require_relative 'caesar_cipher/caesar_cipher'
+require_relative 'number_guess/number_guesser'
+require_relative 'word_guess/word_guess'
 
 enable :sessions
 
-get '/' do
+get '/' dor
   erb :index
 end

a9f84ec4852b1234ff6a7f29c5df2a47f7db4f20
David Adams <david.riding.rails@gmail.com>
2019-01-28 15:32:53 -0700

    Add word guess to Sinatra envelope files

--- exercises.rb
+++ exercises.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require_relative 'caesar_cipher/application'
 require_relative 'number_guess/application'
-# require_relative 'word_guess/application'
+require_relative 'word_guess/application'
 
 enable :sessions
 
